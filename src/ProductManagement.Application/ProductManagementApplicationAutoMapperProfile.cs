452e7d02868b709a5cbbd95ea817668f9eaf2489
wangwenjun <846798937@qq.com>
2023-07-24 09:12:42 +0800

    first

---
+++ src/ProductManagement.Application/ProductManagementApplicationAutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProductManagement.Categories;
+using ProductManagement.Categorys;
+using ProductManagement.Products;
+namespace ProductManagement;
+
+public class ProductManagementApplicationAutoMapperProfile : Profile
+{
+    public ProductManagementApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+        CreateMap<Product, ProductDto>();
+        CreateMap<CreateUpdateProductDto, Product>();
+        CreateMap<Category,CategoryLookupDto>();
+        
+    }
+}
---
+++ test/ProductManagement.Application.Tests/Products/ProductAppService_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace ProductManagement.Products
+{
+    public class ProductAppService_Tests : ProductManagementApplicationTestBase
+    {
+        private readonly IProductAppService _productAppService;
+        public ProductAppService_Tests()
+        {
+            _productAppService = GetRequiredService<IProductAppService>();
+        }
+        /* Todo test methods */
+        [Fact]
+        public async Task  Should_Get_Product_List()
+        {
+            //act
+            var output = await _productAppService.GetListAsync(new PagedAndSortedResultRequestDto());
+            //Assert
+            output.TotalCount.ShouldBe(3);
+            output.Items.ShouldContain(x => x.Name.Contains("Acme Monochrome Laser Printer"));
+
+        }
+
+    }
+}
