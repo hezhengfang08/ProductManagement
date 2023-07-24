9df595f3fa8ba83514cfdf4888718f9fef0fb56c
hezhengfang08 <hezhengfang08@163.com>
2023-07-24 10:35:04 +0800

    tt

---
+++ src/ProductManagement.Application.Contracts/Categories/ICategoriesService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace ProductManagement.Categories
+{
+    public interface ICategoriesService: IApplicationService
+    {
+        Task<PagedResultDto<CategoryLookupDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+    }
+}
---
+++ src/ProductManagement.Application/Categories/CategoriesService.cs
@@ -0,0 +1,42 @@
+﻿using ProductManagement.Categorys;
+using ProductManagement.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Domain.Repositories;
+
+namespace ProductManagement.Categories
+{
+    internal class CategoriesService : ProductManagementAppService, ICategoriesService
+    {
+        private readonly IRepository<Category, Guid> _categoryRepository;
+       public CategoriesService(IRepository<Category, Guid> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;   
+        }
+
+
+        public async Task<PagedResultDto<CategoryLookupDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            var queryable = await _categoryRepository.WithDetailsAsync();
+            queryable = queryable
+               .Skip(input.SkipCount)
+               .Take(input.MaxResultCount)
+               .OrderBy(input.Sorting ?? nameof(Product.Name));
+            var categories = await AsyncExecuter.ToListAsync(queryable);
+            var count = await _categoryRepository.GetCountAsync();
+            return new PagedResultDto<CategoryLookupDto>(
+            count,
+            ObjectMapper.Map<List<Category>, List<CategoryLookupDto>>
+           (categories)
+            );
+        }
+    }
+}
